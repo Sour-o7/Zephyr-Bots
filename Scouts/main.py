a3883d8b2f296f62dc80ca20fa9c3bc123fbcf86
Sour3 <31393967+Sour-o7@users.noreply.github.com>
2024-03-01 13:45:37 -0800

    Create main.py

---
+++ Scouts/main.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from discord.ext import commands
+import discord
+import os.path
+import os
+import threading
+
+#Scouts Version 2.0.0
+#9/25/2022
+
+client = commands.Bot(command_prefix="++", intents=discord.Intents.all())
+
+async def on_ready():
+    print("Online")
+    # Load cogs
+    if client.extensions == {}:
+        print("Loading Cogs...")
+        for f in os.listdir("./cogs"):
+            if f.endswith(".py"):
+                await client.load_extension("cogs." + f[:-3])
+                print(f"Loaded {f[:-3]}")
+        print("Loaded All Cogs")
+client.run('TOKEN')
